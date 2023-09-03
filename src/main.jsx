5370a9928bf49d98024386fddfc7412957ce0ce5
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-09-04 02:47:08 +0530

    Change typography font-family

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./main.css"
+import { AuthProvider } from './Context/AuthContext.jsx'
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "Source Sans Pro",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ThemeProvider>,
 )
