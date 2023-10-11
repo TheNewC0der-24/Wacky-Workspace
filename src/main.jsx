00e9e908bf3e4475bf7c3886c9d1a613ebaedf3a
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-10-12 02:08:45 +0530

    Improve code

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./main.css"
 import { AuthProvider } from './Context/AuthContext.jsx'
+import { theme } from "./Theme/theme.js";
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
-const theme = createTheme({
-  typography: {
-    allVariants: {
-      fontFamily: "Source Sans Pro",
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
