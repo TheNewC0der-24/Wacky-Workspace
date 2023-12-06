db5d061eb04f7ec1dccc4f97b9cd38407f3a1c15
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-12-06 19:41:42 +0530

    Configure firebase app, auth, database and storage

--- src/Config/FirebaseConfig.js
+++ src/Config/FirebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const authDomain = import.meta.env.VITE_AUTH_DOMAIN;
@@ -21,6 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 
 
